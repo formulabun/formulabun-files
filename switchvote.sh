0ba7a58c8bf14b283a12aa9ae05b5888576f6155
Fl_GUI <flor.guilini@hotmail.com>
2022-01-13 10:43:35 +0100

    vote screen switching

---
+++ switchvote.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+dir="load"
+
+fileA="k_formulabun_vote_A.wad"
+fileB="k_formulabun_vote_B.wad"
+
+if [ -L $dir/$fileA ]
+then
+  rm $dir/$fileA
+  ln -s $fileB $dir
+else
+then
+  rm $dir/$fileB
+  ln -s $fileA $dir
+fi
